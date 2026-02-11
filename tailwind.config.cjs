886a382aa4213193b99dddc6ab3ce045aa6f3699
Katsura34 <jesonbeltra22222@gmail.com>
2026-02-11 22:05:15 +0800

    Add Tailwind CSS configuration file

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './resources/views/**/*.blade.php',
+    './resources/js/**/*.js',
+    './resources/css/**/*.css',
+    './public/index.php',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

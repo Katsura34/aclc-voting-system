d83f2162e6ae64cef5b21067f6071f09520cce8e
Katsura34 <jesonbeltra22222@gmail.com>
2026-02-21 20:57:18 +0800

    Auto-commit: routes/web.php at 2/21/2026, 8:57:17 PM

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-Ro
+Route::
 
 // Protected Routes
 Route::middleware(['auth'])->group(function () {

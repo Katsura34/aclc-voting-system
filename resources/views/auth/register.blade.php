74d13808a2da35d7bcfea2ee5ca5867e76fecefb
Katsura34 <jesonbeltra22222@gmail.com>
2026-02-21 21:32:19 +0800

    Auto-commit: resources/views/auth/register.blade.php at 2/21/2026, 9:32:19 PM

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,16 @@
     <title>Register</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="bg-black-5">
-   
+<body class="bg-gray-100 min-h-screen flex items-center justify-center">
+   .<div class="flex flex-col items-center bg-white p-8 rounded-lg shadow-md w-full max-w-md">
+        <h2 class="text-2xl font-bold mb-6">Create an Account</h2>
+       <form>
+            <input type = "text"  placeholder="School Name" class="form-control" >
+            <input type = "text"  placeholder="First Name" >
+            <input type = "text"  placeholder="Last Name" >
+            <input type = "email"  placeholder="Email Address" >
+            <input type = "password"  placeholder="Password" >
+       </form>
+   </div>
 </body>
 </html>

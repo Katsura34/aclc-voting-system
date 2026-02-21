4eee59f8283fbbed322b51db88c2805a321ad31d
Katsura34 <jesonbeltra22222@gmail.com>
2026-02-21 22:07:10 +0800

    Auto-commit: resources/views/admin/users/index.blade.php at 2/21/2026, 10:07:10 PM

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -45,6 +45,12 @@
                 {{-- <a href="{{ route('admin.users.download-template') }}" class="btn btn-info">
                     <i class="bi bi-download"></i> Download Template
                 </a> --}}
+                    {{-- <a href="{{ route('admin.users.download-template') }}" class="btn btn-info">
+                        <i class="bi bi-download"></i> Download Template
+                    </a> --}}
+                    <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#importModal">
+                        <i class="bi bi-upload"></i> Export CSV
+                    </button>
                 {{-- <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#resetAllVotesModal">
                     <i class="bi bi-arrow-counterclockwise"></i> Reset All Votes
                 </button> --}}
